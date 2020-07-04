3ac426b8b387bd67dff063fad08e57c5e370c7ef
emc <micha3l_255@yahoo.com>
2020-07-04 12:45:55 -0500

    Updates

---
+++ precache-manifest.b5137f20d19e403010887382dd4f74a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "702d3e3e7ce1e90875e4c22350ef31a0",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "e9950b931c36ed58edae",
+    "url": "/weather/static/js/2.ac497726.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather/static/js/2.ac497726.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d48c42cfdb593b5e765",
+    "url": "/weather/static/js/main.e8add23c.chunk.js"
+  },
+  {
+    "revision": "65a135568c88d392bf25",
+    "url": "/weather/static/js/runtime-main.d8725fa2.js"
+  }
+]);
